b4e27c12edfa705a35d9a15e31ac355b826b605b
Shehan Tissera <tistuslabs@gmail.com>
2022-10-15 22:39:48 -0700

    Done setting up the vue project on the express project

--- server.js
+++ server.js
@@ -7,13 +7,17 @@ const commonController = require('./controllers/common.controller');
 const app = express();
 // define the port in a variable incase if we need to change it in the future
 const port = 3000;
+// defining the index of the built project
+const path = __dirname + '/app/dist/';
+// separate the routes and the functionality to different files for futre ease of maintainance and improvements
+const beerRouter = require('./routes/beer.route');
 
 // configuring express to use body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// mounting the middleware interceptor for all incoming requests
-app.use(async (req, res, next) => {
+// mounting the middleware to intercept for all incoming requests to /api path
+app.use("/api/", async (req, res, next) => {
     // record the request in the logs
     await commonController.LogRequest(req);
 
@@ -29,9 +33,16 @@ app.use(async (req, res, next) => {
     }
 })
 
-// separate the routes and the functionality to different files for futre ease of maintainance and improvements
-const beerRouter = require('./routes/beer.route');
+// setting the beer router to the express app
 app.use('/api/beer', beerRouter);
+
+// setting the static webpage to express
+app.use(express.static(path));
+
+// host the index page to the get method
+app.get('/', (req, res) => {
+    res.sendFile(path + "index.html");
+});
 
 app.listen(port, async () => {
     try {
